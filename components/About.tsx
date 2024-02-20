a916e08370145f1e40e1a05e00525a33e049da17
pasan1227 <pasanratnayake@gmail.com>
2024-02-20 16:53:28 +0530

    Content updates

--- components/About.tsx
+++ components/About.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import React from "react";
-import { motion } from "framer-motion";
+import React from 'react';
+import { motion } from 'framer-motion';
 
-import SectionHeading from "./SectionHeading";
-import { useSectionInView } from "@/lib/hooks";
+import SectionHeading from './SectionHeading';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
-  const { ref } = useSectionInView("About");
+  const { ref } = useSectionInView('About');
 
   return (
     <motion.section
@@ -21,27 +21,26 @@ export default function About() {
       <SectionHeading>About me</SectionHeading>
 
       <p className="mb-3">
-        I'm a final-year{" "}
+        I'm a final-year{' '}
         <span className="font-medium">
           Computer Science undergraduate at Staffordshire University (UK)
         </span>
-        , and currently learning{" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
+        , and currently learning{' '}
+        <span className="font-medium">full-stack web development</span>.{' '}
         <span className="italic">My favorite part of programming</span> is the
         problem-solving aspect. I <span className="underline">love</span> the
         feeling of finally figuring out a solution to a problem. My core stack
-        is{" "}
+        is{' '}
         <span className="font-medium">
           React, Next.js, Node.js, and MongoDB
         </span>
-        . I am always looking to learn new technologies. I am currently looking
-        for a <span className="font-medium">full-time position</span> as a
-        trainee / associate software engineer.
+        . I am always looking to learn new technologies. I am currently working
+        as a <span className="font-medium">Trainee Software Engineer</span>.
       </p>
 
       <p>
         <span className="italic">When I'm not coding</span>, I enjoy playing
-        video games, watching movies, and playing with my dog. I also enjoy{" "}
+        video games, watching movies, and playing with my dog. I also enjoy{' '}
         <span className="font-medium">learning new things</span>.
       </p>
     </motion.section>
